d80c8e17bb930cbd54277bb8ef9b2d15be03a602
Joseph Li <joseph@labeleven.dev>
2024-01-23 16:31:30 +1100

    fix: try to catch the jito searcher client error

--- src/mempool.ts
+++ src/mempool.ts
@@ -25,7 +25,7 @@ type MempoolUpdate = {
 const getProgramUpdates = (searcherClient: searcher.SearcherClient) =>
   searcherClient.programUpdates(PROGRAMS_OF_INTEREST, [], (error) => {
     logger.error({ error }, 'programUpdates error');
-    throw error;
+    // throw error;
   })
 
 async function* mempool(): AsyncGenerator<MempoolUpdate> {
